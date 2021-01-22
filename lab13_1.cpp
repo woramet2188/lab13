6f4ce80534f2db70134394788e3348568589f9c9
woramet2188 <76035801+woramet2188@users.noreply.github.com>
2021-01-22 15:01:30 +0700

    Initial commit

---
+++ lab13_1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+template <typename T>
+void insertionSort(T d[],int N){
+
+}
+
+int main(){
+	int a[10] = {12,25,30,44,2,0,4,7,55,25};
+	cout << "Input Array:";
+	for(int i = 0; i < 10; i++) cout << a[i] << " ";
+	cout << "\n\n";
+	
+	insertionSort(a,10);
+	
+	cout << "\nSorted Array:";
+	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
+}
